c3df2d8b1fc1b9647be9bbd5aee91950f839c4b5
Guardianff <103484627+Guardianff@users.noreply.github.com>
2024-12-19 21:16:26 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -84,7 +84,7 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
             else:
                 max_results = int(MAX_B_TN)
      query = re.sub(r"[-:\"';!]", " ", query)
-    query = re.sub(r"\s+", " ", query).strip()
+     query = re.sub(r"\s+", " ", query).strip()
     #if filter:
         #better ?
         #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
